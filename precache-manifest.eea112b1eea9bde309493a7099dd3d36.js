00485be3e803552d36d367a1f65adb714f499f76
Demian Floreani <demian.floreani@henshingroup.com>
2022-02-23 10:27:22 +0100

    Updates

---
+++ precache-manifest.eea112b1eea9bde309493a7099dd3d36.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86b60ef929027048f7c4b35f30b51b48",
+    "url": "/dante-finance-gui/index.html"
+  },
+  {
+    "revision": "72b633db345275c26880",
+    "url": "/dante-finance-gui/static/css/main.b8d771e6.chunk.css"
+  },
+  {
+    "revision": "7494fc82b46b24724122",
+    "url": "/dante-finance-gui/static/js/0.7d4694a4.chunk.js"
+  },
+  {
+    "revision": "07ccf49a5c96ca974ece",
+    "url": "/dante-finance-gui/static/js/1.e5703e03.chunk.js"
+  },
+  {
+    "revision": "950fa472651fb0dc8fe9",
+    "url": "/dante-finance-gui/static/js/10.5032d4f2.chunk.js"
+  },
+  {
+    "revision": "2268858ff041853716c3",
+    "url": "/dante-finance-gui/static/js/11.9eb1b236.chunk.js"
+  },
+  {
+    "revision": "800c8c88b71490b35d9e636bbf8c583d",
+    "url": "/dante-finance-gui/static/js/11.9eb1b236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d29fc6c051df750be813",
+    "url": "/dante-finance-gui/static/js/12.2815a0e8.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/dante-finance-gui/static/js/12.2815a0e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25e74bb7995dc91b1f38",
+    "url": "/dante-finance-gui/static/js/13.7236ad27.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/dante-finance-gui/static/js/13.7236ad27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b81a1d82c09840a011b",
+    "url": "/dante-finance-gui/static/js/14.671ec6fc.chunk.js"
+  },
+  {
+    "revision": "467bae7d0ec66b24395b",
+    "url": "/dante-finance-gui/static/js/15.6c5476b7.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/dante-finance-gui/static/js/15.6c5476b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "292e91cae95492eeb912",
+    "url": "/dante-finance-gui/static/js/16.d52038aa.chunk.js"
+  },
+  {
+    "revision": "3cd8944810905fc97c66",
+    "url": "/dante-finance-gui/static/js/17.376ffc9e.chunk.js"
+  },
+  {
+    "revision": "9f1b8351d003c6e86aa3",
+    "url": "/dante-finance-gui/static/js/4.888aca20.chunk.js"
+  },
+  {
+    "revision": "b5e56805f96973820848adb65ddff07d",
+    "url": "/dante-finance-gui/static/js/4.888aca20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fd550c63f7e1478e43c",
+    "url": "/dante-finance-gui/static/js/5.d81fe848.chunk.js"
+  },
+  {
+    "revision": "8e9828568d0b663f3919",
+    "url": "/dante-finance-gui/static/js/6.11065bc4.chunk.js"
+  },
+  {
+    "revision": "1bf5c84d0c626db659cb",
+    "url": "/dante-finance-gui/static/js/7.eb8d8e43.chunk.js"
+  },
+  {
+    "revision": "47c208eab7dd0d857636",
+    "url": "/dante-finance-gui/static/js/8.b0c552c0.chunk.js"
+  },
+  {
+    "revision": "adffe39f78800f89a51e",
+    "url": "/dante-finance-gui/static/js/9.30faeadf.chunk.js"
+  },
+  {
+    "revision": "72b633db345275c26880",
+    "url": "/dante-finance-gui/static/js/main.15eb1a03.chunk.js"
+  },
+  {
+    "revision": "7a26b145be9bfffac655",
+    "url": "/dante-finance-gui/static/js/runtime-main.b03ff1b6.js"
+  },
+  {
+    "revision": "385b2e844755c43a7bf26dbc7de55fd2",
+    "url": "/dante-finance-gui/static/media/Augusta.385b2e84.ttf"
+  },
+  {
+    "revision": "3e2a7cb9ae0b9d30b3935e0e88de247d",
+    "url": "/dante-finance-gui/static/media/HOME1900x1080.3e2a7cb9.png"
+  },
+  {
+    "revision": "f3331ee892233fc83932c9e5873c1523",
+    "url": "/dante-finance-gui/static/media/crypto_tomb_bond.f3331ee8.svg"
+  },
+  {
+    "revision": "4d4f8ca6b4b157510401c5258739104e",
+    "url": "/dante-finance-gui/static/media/crypto_tomb_cash.f2b44ef4.4d4f8ca6.png"
+  },
+  {
+    "revision": "f2b44ef4a644ec61342c200db3380871",
+    "url": "/dante-finance-gui/static/media/crypto_tomb_cash.f2b44ef4.svg"
+  },
+  {
+    "revision": "14510c1a5487c2d5176fe427ef6bda7f",
+    "url": "/dante-finance-gui/static/media/crypto_tomb_share.bf1a6c52.14510c1a.png"
+  },
+  {
+    "revision": "bf1a6c521de2fa79f8d24213632e648d",
+    "url": "/dante-finance-gui/static/media/crypto_tomb_share.bf1a6c52.svg"
+  },
+  {
+    "revision": "14e0049c752600fde1027e60e9f2d7e3",
+    "url": "/dante-finance-gui/static/media/dante.14e0049c.svg"
+  },
+  {
+    "revision": "21889a21808e54e1b99c5cfce307a43c",
+    "url": "/dante-finance-gui/static/media/dante_tomb.21889a21.svg"
+  },
+  {
+    "revision": "6b525b753aad9337f9b9c2ca731974b7",
+    "url": "/dante-finance-gui/static/media/dbond.6b525b75.svg"
+  },
+  {
+    "revision": "5f33a495a08fa7b894087932ec386e7b",
+    "url": "/dante-finance-gui/static/media/discord.5f33a495.svg"
+  },
+  {
+    "revision": "00e4ee2880958b6988f7e2929c6eaa81",
+    "url": "/dante-finance-gui/static/media/ftm_logo_blue.00e4ee28.svg"
+  },
+  {
+    "revision": "45e6a6aa7aa06f2998f946cf38cd9bd5",
+    "url": "/dante-finance-gui/static/media/grail.45e6a6aa.svg"
+  },
+  {
+    "revision": "7bd7d2251cd60525bf700317a2c7324b",
+    "url": "/dante-finance-gui/static/media/grail_ftm.7bd7d225.svg"
+  },
+  {
+    "revision": "042cb5a6cbac5284edd348e07126fbb8",
+    "url": "/dante-finance-gui/static/media/masonry.042cb5a6.png"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/dante-finance-gui/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "268bf98426e621fb6c1f3b6db9970d9d",
+    "url": "/dante-finance-gui/static/media/pit.268bf984.png"
+  },
+  {
+    "revision": "f27e2f894aa8fa407068e64e5c70123e",
+    "url": "/dante-finance-gui/static/media/purgatory.f27e2f89.png"
+  },
+  {
+    "revision": "b9559c2c1d7477268c82bf3a12058bc1",
+    "url": "/dante-finance-gui/static/media/regulations_bg.b9559c2c.png"
+  },
+  {
+    "revision": "28146bd816fad4b71ed4294a177c7662",
+    "url": "/dante-finance-gui/static/media/telegram.28146bd8.svg"
+  },
+  {
+    "revision": "08d2ade8e1a89d1326b8a84d73c4cc6b",
+    "url": "/dante-finance-gui/static/media/twitter.08d2ade8.svg"
+  },
+  {
+    "revision": "d1f736120a29462726fb25a8e88cffba",
+    "url": "/dante-finance-gui/static/media/wallet-connect.d1f73612.svg"
+  },
+  {
+    "revision": "503d2e2264092df45d4d9795b0996855",
+    "url": "/dante-finance-gui/static/media/youtube.503d2e22.svg"
+  }
+]);
