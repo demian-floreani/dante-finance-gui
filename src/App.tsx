6328731d00b7b8a5c970fffe8adf9138409f7077
Demian Floreani <demian.floreani@henshingroup.com>
2022-03-02 13:51:46 +0100

    Use browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { Suspense, lazy } from 'react';
 import { Provider } from 'react-redux';
-//import { BrowserRouter as Router, HashRouter, Route, Switch } from 'react-router-dom';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ThemeProvider as TP } from '@material-ui/core/styles';
 import { ThemeProvider as TP1 } from 'styled-components';
 import { UseWalletProvider } from 'use-wallet';
@@ -41,7 +40,7 @@ const App: React.FC = () => {
 
   return (
     <Providers>
-      <HashRouter>
+      <Router>
         <Suspense fallback={<Loader />}>
           <Switch>
             <Route exact path="/">
@@ -64,7 +63,7 @@ const App: React.FC = () => {
             </Route>
           </Switch>
         </Suspense>
-      </HashRouter>
+      </Router>
     </Providers>
   );
 };
